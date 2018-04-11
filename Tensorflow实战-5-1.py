39430098fb64655a2371020e87405c5985221f41
wangheng <673602165@qq.com>
2018-04-11 18:43:39 +0800

    sdf

--- Tensorflow实战-5-1.py
+++ Tensorflow实战-5-1.py
@@ -4,4 +4,89 @@
 """
 from models.tutorials.image.cifar10 import cifar10,cifar10_input
 import tensorflow as tf
+import numpy as np
+import time
 
+#训练轮数
+max_step = 3000
+batch_size = 128
+data_dir = '/home/wangheng/PycharmProjects/tensorflow/cifar-10-batches-py'
+'''
+    初始化weight函数
+'''
+def variable_with_weight_loss(shape, stddev, w1):
+    var = tf.Variable(tf.truncated_normal(shape, stddev=stddev))
+    if w1 is not None:
+        weight_loss = tf.multiply(tf.nn.l2_loss(var), w1, name="weight_loss")
+        tf.add_to_collection('losses', weight_loss)
+    return var
+"""
+    下载数据集并解压
+"""
+cifar10.maybe_download_and_extract()
+
+images_train, labels_train = cifar10_input.distorted_inputs(data_dir=data_dir, batch_size=batch_size)
+images_test, labels_test = cifar10_input.inputs(eval_data=True, data_dir=data_dir, batch_size=batch_size)
+
+image_holder = tf.placeholder(tf.float32, [batch_size, 24, 24, 3])
+label_holder = tf.placeholder(tf.int32, [batch_size])
+
+"""
+    下面开始创建卷积层
+"""
+weight1 = variable_with_weight_loss(shape=[5, 5, 3, 64], stddev=5e-2, w1=0.0)
+kernel1 = tf.nn.conv2d(image_holder, weight1, strides=[1, 1, 1, 1], padding='SAME')
+bias1 = tf.Variable(tf.constant(0.0, shape=[64]))
+conv1 = tf.nn.relu(tf.nn.bias_add(kernel1, bias1))
+pool1 = tf.nn.max_pool(conv1, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME')
+norm1 = tf.nn.lrn(pool1, 4, bias=1.0, alpha=0.001/9.0, beta=0.75)
+
+"""
+    创建第二个卷积层
+"""
+weight2 = variable_with_weight_loss(shape=[5, 5, 64, 64], stddev=5e-2, w1=0.0)
+kernel2 = tf.nn.conv2d(norm1, weight2, strides=[1, 1, 1, 1], padding='SAME')
+bias2 = tf.Variable(tf.constant(0.1, shape=[64]))
+conv2 = tf.nn.relu(tf.nn.bias_add(kernel2, bias2))
+norm2 = tf.nn.lrn(conv2, 4, bias=1.0, alpha=0.001/9.0, beta=0.75)
+pool2 = tf.nn.max_pool(norm2, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+"""
+    全连接层1
+"""
+reshape = tf.reshape(pool2, [batch_size, -1])
+dim = reshape.get_shape()[1].value
+weight3 = variable_with_weight_loss(shape=[dim, 384], stddev=0.04, w1=0.004)
+bias3 = tf.Variable(tf.constant(0.1, shape=[384]))
+local3 = tf.nn.relu(tf.matmul(reshape, weight3)+bias3)
+
+"""
+    全连接层2
+"""
+weight4 = variable_with_weight_loss(shape=[384, 192], stddev=0.04, w1=0.004)
+bias4 = tf.Variable(tf.constant(0.1, shape=[192]))
+local4 = tf.nn.relu(tf.matmul(local3, weight4) + bias4)
+
+"""
+    全连接层3：用softmax函数计算概率
+"""
+weight5 = variable_with_weight_loss(shape=[192, 10], stddev=1/192.0, w1=0.0)
+bias5 = tf.Variable(tf.constant(0.0, shape=[10]))
+logits = tf.add(tf.matmul(local4, weight5), bias5)
+
+"""
+    最后计算loss
+"""
+def loss(logits, labels):
+    labels = tf.cast(labels, tf.int64)
+    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits,
+                                                                   labels=labels,
+                                                                   name='cross_entropy_per_example')
+    cross_entropy_mean = tf.reduce_mean(cross_entropy, name='cross_entropy')
+
+    tf.add_to_collection('losses', cross_entropy_mean)
+
+    return tf.add_n(tf.get_collection('losses'), name='total_loss')
+
+loss = loss(logits, label_holder)
+train_op = tf.train.AdamOptimizer(1e-3).minimize(loss=loss)
